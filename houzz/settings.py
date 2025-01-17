68758f7a5ed56037d0c5aa8dfceca4420bf7f3d2
fzavala2003 <127550614+fzavala2003@users.noreply.github.com>
2025-01-17 00:04:22 -0500

    Social fixed and Navegator experience added

--- houzz/settings.py
+++ houzz/settings.py
@@ -7,8 +7,6 @@ NEWSPIDER_MODULE = "houzz.spiders"
 
 # USER_AGENT='Mozilla/5.0 (Linux; Android 11; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Mobile Safari/537.36'
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
 
 FEED_FORMAT = 'json'  # Set the output format to JSON
 FEED_URI = 'output.json'  # Set the output file name (you can change 'output.json' to your preferred filename)
@@ -18,3 +16,43 @@ FEED_URI = 'output.json'  # Set the output file name (you can change 'output.jso
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+# ----------- OWN_SETTINGS --------------
+
+
+# Manejo de downloaders con Playwright
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+}
+
+
+PLAYWRIGHT_BROWSER_TYPE = "chromium"  # O "firefox" o "webkit" o "chromium"
+PLAYWRIGHT_LAUNCH_OPTIONS = {
+    "headless": False,
+}
+
+PLAYWRIGHT_CONTEXTS = {
+    "default": {
+        "viewport": {"width": 720, "height": 720},
+    }
+}
+
+
+ROBOTSTXT_OBEY = False
+COOKIES_ENABLED = True
+
+DEFAULT_REQUEST_HEADERS = {
+                'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81",
+                'Upgrade-Insecure-Requests': '1',
+                'Connection': 'keep-alive'
+            }
+
+DOWNLOAD_DELAY = 5
+# settings.py
+
+LOG_FILE = 'scrapy_output.txt'
+""" 
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 60
+"""
--- houzz/spiders/houzz_spider.py
+++ houzz/spiders/houzz_spider.py
@@ -1,14 +1,14 @@
 import time
 from bs4 import BeautifulSoup
 import scrapy
+from scrapy_playwright.page import PageMethod
+from playwright._impl._errors import TimeoutError
 
 from houzz.spiders.utils import extract_emails_from_url
 
 class HouzzSpider(scrapy.Spider):
-    name = "houzz_scraper"
-    
+    name = "houzz_spider"
     base_url="https://www.houzz.com"
-    start_urls = ["https://www.houzz.com/professionals/interior-designer/carter-lake-ia-us-probr0-bo~t_11785~r_4850531"]
 
     custom_settings = {
         'FEEDS': {
@@ -17,48 +17,103 @@ class HouzzSpider(scrapy.Spider):
                 'overwrite': True,  # Set to True to overwrite the file if it already exists
             },
         }
-        }
+    }
 
-    def __init__(self, *args, **kwargs):
-        super(HouzzSpider, self).__init__(*args, **kwargs)
-        self.start_time = time.time()
+    def start_requests(self):
+        houzz_interior_designers_link="https://www.houzz.com/professionals/interior-designer/carter-lake-ia-us-probr0-bo~t_11785~r_4850531"
+        yield scrapy.Request(
+            url=houzz_interior_designers_link,
+            callback=self.parse,
+            meta={
+                "playwright": True, 
+                "playwright_include_page": True,
+                "playwright_page_methods":[PageMethod("wait_for_load_state", "load")]
+            },
+            errback=self.error_handler
+        )
 
-    def parse(self, response):
+    async def parse(self, response):
+        if response.status==200:
+            houzz_interior_designers_page=response.meta["playwright_page"]
+            houzz_interior_designers_page_html=await houzz_interior_designers_page.content()
+            scp=scrapy.Selector(text=houzz_interior_designers_page_html)
+            designers_url_list=scp.css("a.hz-pro-ctl::attr(href)").getall()
+            for i in range(len("designers_url_list")):
+                yield scrapy.Request(
+                    designers_url_list[i], 
+                    callback=self.parse_subpage,
+                    meta={
+                        "playwright": True,  
+                        "playwright_include_page": True,
+                        "playwright_page_methods":[PageMethod("wait_for_load_state", "load")],
+                        "playwright_context": "new"+str(i+1)
+                    },
+                    errback=self.error_handler
+                    
+                )
         
-        target_elements = response.css('a.hz-pro-ctl::attr(href)').extract()
-        for url in target_elements:
-            yield response.follow(url, callback=self.parse_subpage)
-
-        next_page=response.css("a.hz-pagination-link--next::attr(href)").get()
-
+        
+        """ 
+        next_page=scp.css("a.hz-pagination-link--next::attr(href)").get()
         if next_page:
           
-            yield scrapy.Request(self.base_url+next_page, callback=self.parse)
+            yield scrapy.Request(
+                url, 
+                callback=self.parse_subpage,
+                meta={
+                    "playwright": True,  
+                    "playwright_include_page": True,
+                    "playwright_page_methods":[PageMethod("wait_for_load_state", "load")],
+                    "playwright_context": "new"
+                }
+                
+            )
+        """
+    async def parse_subpage(self, response):
+        if(response.status==200):
+            designer_page=response.meta["playwright_page"]
+            designers_page_html=await designer_page.content()
+            soup = BeautifulSoup(designers_page_html, 'html.parser')
+            data = {"url": response.url}
 
-    def parse_subpage(self, response):
-        soup = BeautifulSoup(response.body, 'html.parser')
-        data = {"url": response.url}
+            business_section = soup.find('section', id='business')
+        
+            for div in business_section.find_all('div'):
+                h3 = div.find('h3')
+                if h3:
+                    key = h3.get_text(strip=True)
+                    p = div.find('p')
+                    if p:
+                        value = p.get_text()
+                        data[key] = value
+                        print(key, value)
+                       
+                        if "Find" in value:
+                            network_names=value.split("Find me on ")[1:]
+                            print(network_names)
+                            social_networks_dic={}
+                            links_containers = div.find_all('a')
+                            network_links=[]
+                            for link_container in links_containers:
+                                network_links.append(link_container.get('href'))
+                            
+                            for i in range(len(network_names)):
+                                social_networks_dic[network_names[i]]=network_links[i]
+                            data[key]=social_networks_dic
+            
+            if "Website" in data:
+                found_emails = extract_emails_from_url(data["Website"])
+                data["Emails"] = found_emails
+            
+            await designer_page.close()
+            yield data
+    
+    async def error_handler(self, failure):
+        page=failure.request.meta["playwright_page"]
+        await page.close()
+        if isinstance(failure.value, TimeoutError):
+            print("TimeoutError, url: ", failure.request.url)
 
-        business_section = soup.find('section', id='business')
-      
-        for div in business_section.find_all('div'):
-            h3 = div.find('h3')
-            if h3:
-                key = h3.get_text(strip=True)
-                p = div.find('p')
-                if p:
-                    value = p.get_text()
-                    data[key] = value
 
-        if "Website" in data:
-            found_emails = extract_emails_from_url(data["Website"])
-            data["Emails"] = found_emails
+    
 
-        yield data
-
-
-
-    def closed(self, reason):
-        end_time = time.time()  # Record the end time
-        total_time = end_time - self.start_time
-        self.log(f"Total time taken for scraping: {total_time} seconds")
